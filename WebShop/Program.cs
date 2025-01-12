a45875724edf63dca9a722521a06bd39dd154107
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2025-01-12 04:20:13 +0100

    Initial commit

---
+++ WebShop/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+using WebShop.Data;
+using WebShop.Models;
+
+namespace WebShop
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Shop app = new Shop("Your console game shop");
+            await app.Run();
+
+            // DB PRODUCT SEEDING
+            //await SeedDb.SeedDatabase();
+            //Console.WriteLine("Database seeded sucessfully  ");
+            //Console.WriteLine();
+
+        }
+    }
+}
